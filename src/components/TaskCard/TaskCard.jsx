3d4724d5ea178f123fa6ad5398ac77930c21e55e
NF RIDOY <nfridoy@gmail.com>
2024-01-10 00:54:01 +0600

    Add Task + icons

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,4 +1,5 @@
-
+import { FaLink } from "react-icons/fa6";
+import { FaCalendarAlt } from "react-icons/fa";
 
 export default function TaskCard() {
 
@@ -11,8 +12,28 @@ export default function TaskCard() {
                     <h2 className="text-lg font-bold">User Name: </h2>
                 </div>
                 <p>Details</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <div className="card-actions flex justify-between px-2">
+
+                    <button className="border-2 ">
+                        <span className="flex ">
+                            <span className="pt-2">
+                                <FaLink />
+                            </span>
+                            <span>
+                                10
+                            </span>
+                        </span>
+                    </button>
+                    <button className="">
+                        <span className="flex ">
+                            <span className="pt-2">
+                                <FaCalendarAlt />
+                            </span>
+                            <span className="pt-1">
+                                Date
+                            </span>
+                        </span>
+                    </button>
                 </div>
             </div>
         </div>
--- src/components/TaskContainer/TaskContainer.jsx
+++ src/components/TaskContainer/TaskContainer.jsx
@@ -2,7 +2,7 @@
 
 export default function TaskContainer({ children, mainTitle }) {
     return (
-        <div className="p-2 w-96 bg-base-200 max-h-screen">
+        <div className="p-2 w-96 bg-base-200 max-h-screen rounded-sm">
             <h1 className="pl-5 py-6 mb-3 font-bold  ">
                 {mainTitle}
             </h1>
