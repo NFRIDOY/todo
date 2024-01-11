c11c1f6966ea94f0c53fc8f0f058cf30dd5141ab
NF RIDOY <nfridoy@gmail.com>
2024-01-11 15:18:20 +0600

    axios Deploy

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
-    // baseURL: 'https://todo-server-ashen-nine.vercel.app/api/v1',
+    // baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'https://todo-server-ashen-nine.vercel.app/api/v1',
     withCredentials: true
 })
 
